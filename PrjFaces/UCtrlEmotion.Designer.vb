99f88b463ad234d1189532c39c929a5886393697
veiko vilta <veikovilta@gmail.com>
2024-02-09 15:51:24 +0200

    added pictures and code for image picking

---
+++ PrjFaces/UCtrlEmotion.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UCtrlEmotion
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl1 overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(UCtrlEmotion))
+        Me.pbEmotion = New System.Windows.Forms.PictureBox()
+        Me.imgLoend = New System.Windows.Forms.ImageList(Me.components)
+        CType(Me.pbEmotion, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbEmotion
+        '
+        Me.pbEmotion.Location = New System.Drawing.Point(0, 0)
+        Me.pbEmotion.Name = "pbEmotion"
+        Me.pbEmotion.Size = New System.Drawing.Size(32, 32)
+        Me.pbEmotion.TabIndex = 0
+        Me.pbEmotion.TabStop = False
+        '
+        'imgLoend
+        '
+        Me.imgLoend.ImageStream = CType(resources.GetObject("imgLoend.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.imgLoend.TransparentColor = System.Drawing.Color.Transparent
+        Me.imgLoend.Images.SetKeyName(0, "confused.png")
+        Me.imgLoend.Images.SetKeyName(1, "cool.png")
+        Me.imgLoend.Images.SetKeyName(2, "smile.png")
+        Me.imgLoend.Images.SetKeyName(3, "star.png")
+        Me.imgLoend.Images.SetKeyName(4, "teeth.png")
+        Me.imgLoend.Images.SetKeyName(5, "yell.png")
+        '
+        'UCtrlEmotion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.pbEmotion)
+        Me.Name = "UCtrlEmotion"
+        Me.Size = New System.Drawing.Size(205, 160)
+        CType(Me.pbEmotion, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pbEmotion As PictureBox
+    Friend WithEvents imgLoend As ImageList
+End Class
---
+++ PrjFaces/UCtrlEmotion.vb
@@ -0,0 +1,25 @@
+﻿Public Class UCtrlEmotion
+
+    Private Sub selectMood(ByVal intMood As Integer)
+        pbEmotion.Image = imgLoend.Images(intMood)
+    End Sub
+
+    Public Sub setMood(ByVal strMood As String)
+        Select Case strMood
+            Case "confused"
+                Me.selectMood(0)
+            Case "cool"
+                Me.selectMood(1)
+            Case "smile"
+                Me.selectMood(2)
+            Case "star"
+                Me.selectMood(3)
+            Case "teeth"
+                Me.selectMood(4)
+            Case "yell"
+                Me.selectMood(5)
+            Case Else
+                Me.selectMood(0)
+        End Select
+    End Sub
+End Class
